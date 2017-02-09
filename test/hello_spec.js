314a2baf382837280ed9454fb4875c0bc889fb3b
gmx9308 <gmx9308@gmail.com>
2017-02-09 14:21:25 -0800

    debug

--- test/hello_spec.js
+++ test/hello_spec.js
@@ -1,6 +1,6 @@
-
+var sayHello = require('../src/hello');
 describe("Hello", function () {
-    it("say hello", function () {
-        expect(sayHello()).toBe("Hello, world!");
+    it("says hello", function () {
+        expect(sayHello('Jane')).toBe("Hello, Jane!");
     });
 });
