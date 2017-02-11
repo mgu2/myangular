ab28d75adcb03609335a3ee20530b8fc92706056
gmx9308 <gmx9308@gmail.com>
2017-02-10 17:50:20 -0800

    add $digestonce
    $digestOnce will run each watcher once, when a watcher value changed,
    it is marked dirty and $digestOnce again

--- src/scope.js
+++ src/scope.js
@@ -1,28 +1,34 @@
 'use strict';
 var _ = require('lodash');
 
-function initWatchVal(){}
-
+function initWatchVal() {}
 Scope.prototype.$watch = function (watchFn, listenerFn) {
     var watcher = {
-        watchFn: watchFn,
-        listenerFn: listenerFn,
-        last: initWatchVal
+        watchFn: watchFn
+        , listenerFn: listenerFn || function () {}
+        , last: initWatchVal
     };
     this.$$watchers.push(watcher);
 };
-Scope.prototype.$digest = function () {
+Scope.prototype.$digestOnce = function () {
     var self = this;
-    var newValue, oldValue;
+    var newValue, oldValue, dirty;
     _.forEach(this.$$watchers, function (watcher) {
         newValue = watcher.watchFn(self);
         oldValue = watcher.last;
         if (newValue !== oldValue) {
             watcher.last = newValue;
-            watcher.listenerFn(newValue, oldValue===initWatchVal ? newValue : oldValue, self);
+            watcher.listenerFn(newValue, oldValue === initWatchVal ? newValue : oldValue, self);
         }
-        
+        dirty = true;
     });
+    return dirty;
+};
+Scope.prototype.$digest = function () {
+    var dirty;
+    do {
+        dirty = this.$$digestOnce();
+    } while (dirty);
 };
 
 function Scope() {
